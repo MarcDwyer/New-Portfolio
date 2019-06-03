f5ab57d49a687f4a92589000789be5a12341e5be
Marc Dwyer <marcdwyer1738@gmail.com>
2019-06-03 12:02:15 -0500

    fixed ga

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -5,41 +5,41 @@ import Footer from '../footer/footer'
 
 const projects = [
     {
-    title: 'gopoll.me',
-    details: "This app was inspired by Strawpoll.me. In my opinion a good web app is one that has a simple UI and interactive experience. With the use of WebSockets and the power of Go, I created websocket rooms using gorilla/mux package. With Websocket rooms, I can efficiently broadcast to users viewing specific types of data. In this case, I broadcast only to users viewing the same poll. With the knowledge I've gained from this project, my skills are much more valuable. The ability to broadcast specific messages depenedent on what the client is viewing is incredibly powerful for a variety of situations.",
-    url: 'https://fetchappbucket.s3.us-east-2.amazonaws.com/port/poller.jpg',
-    links: [{title: "Live Demo", link: "https://gopoll.me/"}, {title: "Full-Stack", link: "https://github.com/MarcDwyer/GoPoll"}],
-    stack: "React, Go and MongoDB"
- },
- {
-     title: "Fetcherapp.net v2",
-     details: "This application pulls steamer's status from both the Twitch API as well as the Youtube API. Both APIs have different data structures and the biggest difficulty was merging both data types into one type and serving it to the client. The backend uses Go and makes use of Go's channels, polymorphism and GoRoutines making the application incredibly fast. The Front-end makes use of React, and keeps the client updated using WebSockets and only updates the client when the server has updated the object of streamers. The frontend is equipped with its own notification system, streamer statistics and images of the stream. This application is extremely convenient when tracking streamers from two different platforms",
-     url: "https://fetchappbucket.s3.us-east-2.amazonaws.com/port/twitchu.jpg",
-     links: [{title: "Live Demo", link: "https://www.fetcherapp.net/"}, {title: "Full-Stack", link: "https://github.com/MarcDwyer/theNetwork-react-hooks-typescript"}],
-     stack: "React and Go"
- },
-{
-    title: "Live Chat",
-    details: "SocketIO is an incredibly powerful tool. Allowing multiple users to connect with eachother. I created this app because I feel like its important for a Developer to be able to create software that allow users to communicate.",
-    url: "https://s3.us-east-2.amazonaws.com/fetchappbucket/port/livechat.jpg",
-    links: [{title: "Live Demo", link: "https://livechattesting.herokuapp.com/"}, {title: "Full-Stack", link: "https://github.com/MarcDwyer/Live-Chat"}],
-    stack: "HTML, CSS, JavaScript, NodeJS and SocketIO"
-},
-{
-    title: "My Portfolio Page",
-    details: "This portfolio page uses React! Wasn't exactly necessary to use it but it was fun!",
-    url: "https://s3.us-east-2.amazonaws.com/fetchappbucket/port/port.jpg",
-    links: [{title: "Front-End", link: "https://github.com/MarcDwyer/NewPort"}],
-    stack: "React and Sass"
-}
+        title: 'gopoll.me',
+        details: "This app was inspired by Strawpoll.me. In my opinion a good web app is one that has a simple UI and interactive experience. With the use of WebSockets and the power of Go, I created websocket rooms using gorilla/mux package. With Websocket rooms, I can efficiently broadcast to users viewing specific types of data. In this case, I broadcast only to users viewing the same poll. With the knowledge I've gained from this project, my skills are much more valuable. The ability to broadcast specific messages depenedent on what the client is viewing is incredibly powerful for a variety of situations.",
+        url: 'https://fetchappbucket.s3.us-east-2.amazonaws.com/port/poller.jpg',
+        links: [{ title: "Live Demo", link: "https://gopoll.me/" }, { title: "Full-Stack", link: "https://github.com/MarcDwyer/GoPoll" }],
+        stack: "React, Go and MongoDB"
+    },
+    {
+        title: "Fetcherapp.net v2",
+        details: "This application pulls steamer's status from both the Twitch API as well as the Youtube API. Both APIs have different data structures and the biggest difficulty was merging both data types into one type and serving it to the client. The backend uses Go and makes use of Go's channels, polymorphism and GoRoutines making the application incredibly fast. The Front-end makes use of React, and keeps the client updated using WebSockets and only updates the client when the server has updated the object of streamers. The frontend is equipped with its own notification system, streamer statistics and images of the stream. This application is extremely convenient when tracking streamers from two different platforms",
+        url: "https://fetchappbucket.s3.us-east-2.amazonaws.com/port/twitchu.jpg",
+        links: [{ title: "Live Demo", link: "https://www.fetcherapp.net/" }, { title: "Full-Stack", link: "https://github.com/MarcDwyer/theNetwork-react-hooks-typescript" }],
+        stack: "React and Go"
+    },
+    {
+        title: "Live Chat",
+        details: "SocketIO is an incredibly powerful tool. Allowing multiple users to connect with eachother. I created this app because I feel like its important for a Developer to be able to create software that allow users to communicate.",
+        url: "https://s3.us-east-2.amazonaws.com/fetchappbucket/port/livechat.jpg",
+        links: [{ title: "Live Demo", link: "https://livechattesting.herokuapp.com/" }, { title: "Full-Stack", link: "https://github.com/MarcDwyer/Live-Chat" }],
+        stack: "HTML, CSS, JavaScript, NodeJS and SocketIO"
+    },
+    {
+        title: "My Portfolio Page",
+        details: "This portfolio page uses React! Wasn't exactly necessary to use it but it was fun!",
+        url: "https://s3.us-east-2.amazonaws.com/fetchappbucket/port/port.jpg",
+        links: [{ title: "Front-End", link: "https://github.com/MarcDwyer/NewPort" }],
+        stack: "React and Sass"
+    }
 ]
 const Home = () => {
     return (
         <div>
-        <Intro />
-        <Project project={projects} />
-        <Footer />
-     </div>
+            <Intro />
+            <Project project={projects} />
+            <Footer />
+        </div>
     )
 }
 
--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,31 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './nav_styles.scss'
 import { withRouter } from "react-router";
 import { Link } from 'react-router-dom'
+import ReactGA from 'react-ga';
 
 const Nav = (props) => {
+    useEffect(() => {
+        ReactGA.initialize('UA-132536428-1');
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, [])
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, [props])
     return (
         <div className="parent nav">
-        <div className="naver container">
-        <i className="fa fa-bars" />
-            <div className="navigator">
-                <Link 
-                style={props.location.pathname === "/" ? {borderTop: "1px solid black"} : {}}
-                 to="/" className="fontme"><span>Home</span></Link>
-                <Link 
-                style={props.location.pathname === "/resume" ? {borderTop: "1px solid black"} : {}}
-                to="/resume" className="fontme"><span>Resume</span></Link>
-                <Link 
-                style={props.location.pathname === "/about" ? {borderTop: "1px solid black"} : {}}
-                to="/about" className="fontme"><span>About</span></Link>
-            </div>
-            <div className="icons">
-                <div className="iconset">
-                <a target="_blank" rel="noopener noreferrer" href="https://github.com/MarcDwyer"><i className="fa fa-github"></i></a>
-                <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/marc-dwyer-53087339/"><i className="fa fa-linkedin"></i></a>
+            <div className="naver container">
+                <i className="fa fa-bars" />
+                <div className="navigator">
+                    <Link
+                        style={props.location.pathname === "/" ? { borderTop: "1px solid black" } : {}}
+                        to="/" className="fontme"><span>Home</span></Link>
+                    <Link
+                        style={props.location.pathname === "/resume" ? { borderTop: "1px solid black" } : {}}
+                        to="/resume" className="fontme"><span>Resume</span></Link>
+                    <Link
+                        style={props.location.pathname === "/about" ? { borderTop: "1px solid black" } : {}}
+                        to="/about" className="fontme"><span>About</span></Link>
+                </div>
+                <div className="icons">
+                    <div className="iconset">
+                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/MarcDwyer"><i className="fa fa-github"></i></a>
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/marc-dwyer-53087339/"><i className="fa fa-linkedin"></i></a>
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
     )
 }
